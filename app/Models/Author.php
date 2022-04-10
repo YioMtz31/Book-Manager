3467ca85ab98d2c87a87635360488913d3c0ad38
Yobhani M <yobhani.martinez@gmail.com>
2022-04-10 10:46:17 -0700

    base migrations and models

---
+++ app/Models/Author.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+
+
+      /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+
+    /**
+     * Get the books for the author
+     */
+    public function book()
+    {
+        return $this->hasMany(Book::class);
+    }
+}
---
+++ app/Models/Book.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+          /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'author_id',
+        'category_id',
+        'publication_date'
+    ];
+
+    /**
+     * Get the author associated with the book.
+     */
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    /**
+     * Get the category associated with the book.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
