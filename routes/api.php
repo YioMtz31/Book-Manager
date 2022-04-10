a86fc5efb096fb4ff13bf3bdd70c381f6416f9b4
Yobhani M <yobhani.martinez@gmail.com>
2022-04-10 10:47:52 -0700

    base api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/books',[BookController::class,'index']);
+Route::post('/books',[BookController::class,'store']);
+Route::post('/author',[AuthorController::class,'store']);
+Route::post('/category',[CategoryController::class,'store']);
